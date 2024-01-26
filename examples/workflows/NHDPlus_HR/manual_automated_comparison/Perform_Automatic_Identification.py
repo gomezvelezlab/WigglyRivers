6a7b3b2559af99a77f9112f19d0be3c15d10e450
Daniel Gonzalez-Duque <daniel.gonzalez@vanderbilt.edu>
2024-01-26 10:13:52 -0600

    Add code to perform automatic detection to compare

--- examples/workflows/NHDPlus_HR/manual_automated_comparison/Perform_Automatic_Identification.py
+++ examples/workflows/NHDPlus_HR/manual_automated_comparison/Perform_Automatic_Identification.py
@@ -36,13 +36,13 @@ from scipy.spatial import Delaunay
 from scipy.spatial.distance import euclidean
 
 # Importing pyMeander Packages
-from pyMeander import Rivers
-from pyMeander import RiverFunctions as RF
-from pyMeander import ExtractNHDPlusHRData as NHD
-from pyMeander.utilities import utilities as utl
-from pyMeander.utilities import filesManagement as FM
-from pyMeander import Logger
-from pyMeander import WaveletTreeFunctions as WTFunc
+from WigglyRivers import RiverDatasets
+from WigglyRivers import RiverFunctions as RF
+from WigglyRivers import ExtractNHDPlusHRData as NHD
+from WigglyRivers.utilities import utilities as utl
+from WigglyRivers.utilities import filesManagement as FM
+from WigglyRivers import Logger
+from WigglyRivers import WaveletTreeFunctions as WTFunc
 
 
 # ---------------------
@@ -63,19 +63,21 @@ logger = Logger(console=True)
 # ------------------
 # Parameters
 # ------------------
-path_projects = './projects/old_characterization/corrected/'
+path_projects = f'{os.getcwd()}/examples/workflows/NHDPlus_HR/manual_automated_comparison/meander_comparison/characterization/manual_corrected/'
 projects = utl.get_folders(path_projects)
+projects = list(np.sort(projects))
 path_projects_all = [f'{path_projects}{p}/' for p in projects]
-path_projects_c = './projects/old_characterization/with_automatic/'
+path_projects_c = f'{os.getcwd()}/examples/workflows/NHDPlus_HR/manual_automated_comparison/meander_comparison/characterization/manual_with_automatic/'
 path_projects_out = [f'{path_projects_c}{p}/' for p in projects]
 print(projects)
 # Current projection of information
 projection = 'esri:102003'
 
+
 # ------------------------
 # Calculate automatic
 # ------------------------
-bounds_array_str = 'inflection'
+bounds_array_str = 'extended'
 # Select project to load
 for project in projects:
     try:
@@ -85,11 +87,18 @@ for project in projects:
 
     project_to_load = path_projects_all[i_p]
     print('Set project to load: ' + project_to_load)
-    rivers = Rivers(logger=logger)
-    river_network_file = f'{project_to_load}/rivers.hdf5'
-    rivers.load_river_network(
-        river_network_file,
-        meanders_database_file=f'{project_to_load}/meander_database.csv')
+    rivers = RiverDatasets(logger=logger)
+    river_network_file = f'{project_to_load}/rivers_manual_corrected.hdf5'
+    # Load corrected files
+    try:
+        rivers.load_river_network(
+            river_network_file,
+            fn_meanders_database=f'{project_to_load}/meander_database_manual_corrected.feather')
+    except FileNotFoundError:
+        river_network_file = f'{project_to_load}/rivers_manual.hdf5'
+        rivers.load_river_network(
+            river_network_file,
+            fn_meanders_database=f'{project_to_load}/meander_database_manual.feather')
     id_rivers = rivers.id_values
 
     # Select River
@@ -98,11 +107,11 @@ for project in projects:
     # -----------------------------
     # Calculate Smooth Coordinates
     # -----------------------------
-    rivers[id_river].calculate_smooth(poly_order=2, gaussian_window=1,)
+    # rivers[id_river].calculate_smooth(poly_order=2, gaussian_window=1,)
     # -----------------------------
     # Calculate Curvature
     # -----------------------------
-    rivers[id_river].calculate_curvature(data_source='smooth') 
+    rivers[id_river].calculate_curvature() 
     # -----------------------------
     # Extract CWT tree
     # -----------------------------
@@ -120,9 +129,13 @@ for project in projects:
     # Save database
     # -----------------------------
     rivers.save_rivers(
-        path_out=f'{path_projects_c}{project}/',
+        path_output=f'{path_projects_c}{project}/',
         file_name='rivers_automatic.hdf5',
-        file_name_meander_database=f'meander_database_{bounds_array_str}_all.csv')
+        fn_meander_database=f'meander_database_{bounds_array_str}.csv')
+    rivers.save_rivers(
+        path_output=f'{path_projects_c}{project}/',
+        file_name='rivers_automatic.hdf5',
+        fn_meander_database=f'meander_database_{bounds_array_str}.feather')
 
 
 
