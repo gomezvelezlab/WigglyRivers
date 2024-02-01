Correct meander computation and add wavelength when CWT is calculated

WigglyRivers/rivers/CompareMeanders.py
@@ -39,7 +39,8 @@ from . import RiverFunctions as RF
 # -----------
 # Functions
 # -----------
-def extract_closet_meanders(database_1, database_2, link_x='x_o', link_y='y_o'):
+def extract_closet_meanders(database_1, database_2, link_x='x_o', link_y='y_o',
+                            threshold=0.8):
     """
     Description:
     ------------ 
@@ -69,46 +70,66 @@ def extract_closet_meanders(database_1, database_2, link_x='x_o', link_y='y_o'):
     # Loop through all meanders
     for i_m in range(len(database_1)):
         try:
-            x_o = RF.convert_str_float_list_vector(database_1['x_o'].values[i_m])
+            x_o = RF.convert_str_float_list_vector(database_1[link_x].values[i_m])
         except AttributeError:
-            x_o = database_1['x_o'].values[i_m]
+            x_o = database_1[link_x].values[i_m]
 
         try:
-            y_o = RF.convert_str_float_list_vector(database_1['y_o'].values[i_m])
+            y_o = RF.convert_str_float_list_vector(database_1[link_y].values[i_m])
         except AttributeError:
-            y_o = database_1['y_o'].values[i_m]
+            y_o = database_1[link_y].values[i_m]
         # Save data
         for i in database_1.columns:
             data_to_save[f'{i}_1'] = [database_1[i].values[i_m]]
 
         # Extractr variables
-        comid_o = database_1['comid'].values[i_m]
+        comid_o = database_1['start_comid'].values[i_m]
         curvature_side = database_1['curvature_side'].values[i_m]
         # Extract coordinates from auto that have the same comid
-        sub_df = database_2[database_2['comid'] == comid_o]
+        sub_df = database_2[database_2['start_comid'] == comid_o]
         # Extract same curvature side
-        sub_df = sub_df[sub_df['curvature_side'] == curvature_side]
-        if len(sub_df) == 0:
-            sub_df = database_2[database_2['comid'] == comid_o]
-            if len(sub_df) == 0:
-                sub_df = copy.deepcopy(database_2)
+        # sub_df = sub_df[sub_df['curvature_side'] == curvature_side]
+        # if len(sub_df) == 0:
+        #     sub_df = database_2[database_2['comid'] == comid_o]
+        #     if len(sub_df) == 0:
+        #         sub_df = copy.deepcopy(database_2)
 
-        
         # Extract coordinates from auto
+        # Find starting and ending points close to the manual meander
+        points_st_o = np.array([x_o[0], y_o[0]])
+        points_end_o = np.array([x_o[-1], y_o[-1]])
+        points_st_a = np.array([
+            sub_df['x_start'].values, sub_df['y_start'].values]).T
+        points_end_a = np.array([
+            sub_df['x_end'].values, sub_df['y_end'].values]).T
+        # Calculate distance
+        dist_st = np.linalg.norm(points_st_a - points_st_o, axis=1)
+        dist_end = np.linalg.norm(points_end_a - points_end_o, axis=1)
+
+        i_sort_st = np.argsort(dist_st)
+        i_sort_end = np.argsort(dist_end)
+        # TODO: Check this calculation taking into account the inflection points
+
+        # pick the first meanders to compare
+        pick = 2
+        i_compare = pd.unique(np.concatenate(
+            [i_sort_st[:pick], i_sort_end[:pick]]))
+        sub_df = sub_df.iloc[i_compare]
+        # Find the meanders that intersect the most
         len_largest = 0
         selected_m = 0
         for i_sub in range(len(sub_df)):
             try:
                 x_a = RF.convert_str_float_list_vector(
-                    sub_df['x_o'].values[i_sub])
+                    sub_df[link_x].values[i_sub])
             except AttributeError:
-                x_a = sub_df['x_o'].values[i_sub]
+                x_a = sub_df[link_x].values[i_sub]
                 
             try:
                 y_a = RF.convert_str_float_list_vector(
-                    sub_df['y_o'].values[i_sub])
+                    sub_df[link_y].values[i_sub])
             except AttributeError:
-                y_a = sub_df['y_o'].values[i_sub]
+                y_a = sub_df[link_y].values[i_sub]
 
             idx_int_x = np.intersect1d(x_o, x_a)
             idx_int_y = np.intersect1d(y_o, y_a)
@@ -120,14 +141,15 @@ def extract_closet_meanders(database_1, database_2, link_x='x_o', link_y='y_o'):
 
         try:
             x_s = RF.convert_str_float_list_vector(
-                sub_df['x_o'].values[selected_m])
+                sub_df[link_x].values[selected_m])
         except AttributeError:
-            x_s = sub_df['x_o'].values[selected_m]
+            x_s = sub_df[link_x].values[selected_m]
         # Save the selected meander
         for i in sub_df.columns:
             data_to_save[f'{i}_2'] = [sub_df[i].values[selected_m]]
         # Perform classification
-        class_value, f_oa, f_om = classify_meanders(x_o, x_s)
+        class_value, f_oa, f_om = classify_meanders(
+            x_o, x_s, threshold=threshold)
         data_to_save['Zone'] = [class_value]
         data_to_save['f_oa'] = [f_oa]
         data_to_save['f_om'] = [f_om]
@@ -141,7 +163,7 @@ def extract_closet_meanders(database_1, database_2, link_x='x_o', link_y='y_o'):
     return database
 
 
-def classify_meanders(manual_indices, auto_indices):
+def classify_meanders(manual_indices, auto_indices, threshold=0.8):
     """
     Description:
     ------------
@@ -173,18 +195,18 @@ def classify_meanders(manual_indices, auto_indices):
     f_om = len(fst) / len(manual_indices)
 
     # Classification
-    if f_oa >= 0.8:
-        if f_om >= 0.8:
+    if f_oa >= threshold:
+        if f_om >= threshold:
             # Inside (I)
             classification_value = 1
-        elif f_om < 0.8:
+        elif f_om < threshold:
             # Underestimated (II)
             classification_value = 2
     else:
-        if f_om >= 0.8:
+        if f_om >= threshold:
             # Overestimated (III)
             classification_value = 3
-        elif f_om < 0.8:
+        elif f_om < threshold:
             # Outside (IV)
             classification_value = 4
     return classification_value, f_oa, f_om

WigglyRivers/rivers/RiverFunctions.py
@@ -791,7 +791,8 @@ def fit_splines_complete(data, method='geometric_mean', ds=0,
         'so': stream order
     :param method: str,
         Method to calculate the distance between points.
-        Options: 'min', 'geometric_mean', 'mean', and 'width_based'
+        Options: 'min', 'geometric_mean', 'mean', 'geometric_mean_width',
+        and 'min_width'
     :return:
         data: dict,
             Dictionary with the data. The dictionary must include the following
@@ -818,12 +819,15 @@ def fit_splines_complete(data, method='geometric_mean', ds=0,
     # ----------------------------------------
     # Calculate geometric meand of the width
     # ----------------------------------------
-    # w_gm = 10 ** np.mean(np.log10(w))
+    w_gm = 10 ** np.mean(np.log10(w))
     w_value = np.nanmin(w)
-    if method == 'width_based' and not(np.isnan(w_value)):
+    if method == 'min_width' and not(np.isnan(w_value)):
         method = 'geometric_mean'
         ds = w_value
-    elif method == 'width_based' and np.isnan(w_value):
+    elif method == 'geometric_mean_width' and not(np.isnan(w_gm)):
+        method = 'geometric_mean'
+        ds = w_gm
+    elif method in ['min_width', 'geometric_mean_width'] and np.isnan(w_value):
         raise ValueError('The width value is NaN')
     # -------------------
     # Get coordinate poly

WigglyRivers/rivers/RiversNetwork.py
@@ -1099,6 +1099,7 @@ class RiverDatasets:
             #     start_comid = float(hw)
             # except:
             #     start_comid = None
+            start_comid = hw
             
             if scale_by_width is None:
                 try:
@@ -1327,11 +1328,16 @@ class RiverTransect:
                             'method': str, method to calculate the distance
                                 between points. Options include:
                                 'min', 'mean', 'geometric_mean',
-                                'gemoetric_mean_width'.
-                                Default is 'geometric_mean'.
+                                'geometric_mean_width', 'min_width'.
+                                The shorter the distance the heavier the file
+                                will be.
+                                Default is 'min_width'.
                             'ds': Set distance to perorm the resampling.
                                 If this value is None the distance will be
-                                calculated using the method provided.
+                                calculated using the method provided. This
+                                distance will overide the method provided.
+                                The shorter the distance the heavier the file
+                                will be.
                                 Default is None.
                             'k': int, polynomial order of the spline.
                                 Default is 3.
@@ -1474,7 +1480,7 @@ class RiverTransect:
         self.scaled_data = False
 
         self.kwargs_resample_default = {
-            'method': 'width_based',
+            'method': 'min_width',
             'ds': 0,
             'k': 3,
             'smooth': 0.0,
@@ -2763,6 +2769,7 @@ class RiverTransect:
             x_inf = database_meanders[f'x_inf'].iloc[i]
             y_inf = database_meanders[f'y_inf'].iloc[i]
             s_inf = database_meanders[f's_inf'].iloc[i]
+            wavelength = database_meanders['wavelength_c'].iloc[i]
             if inflection_flag:
                 if clip.lower() == 'downstream':
                     if meander_id_prev != meander_id:
@@ -2805,7 +2812,8 @@ class RiverTransect:
                 self.add_meander(meander_id, ind_start, ind_end, 
                                 sk=sk, fl=fl, automatic_flag=1,
                                 inflection_flag=inflection_flag, tree_id=tree_id,
-                                x_inf=x_inf, y_inf=y_inf, s_inf=s_inf)
+                                x_inf=x_inf, y_inf=y_inf, s_inf=s_inf,
+                                wavelength=wavelength)
             except SmallMeanderError:
                 self.logger.warning('Small Meander detected. Removing...')
                 continue
@@ -2813,6 +2821,33 @@ class RiverTransect:
             meander_id_prev = copy.deepcopy(meander_id)
             meander_id += 1
             ind_end_prev = ind_end
+            # Test the relationship between lambda and lambda_hm
+            # TODO: Remove these lines
+            # x_m = database_meanders['x'].iloc[i]
+            # y_m = database_meanders['y'].iloc[i]
+            # x = self.meanders[meander_id - 1].x_inf
+            # y = self.meanders[meander_id - 1].y_inf
+            # x_inf = x[self.meanders[meander_id - 1].ind_inf_st:self.meanders[meander_id - 1].ind_inf_end + 1]
+            # y_inf = y[self.meanders[meander_id - 1].ind_inf_st:self.meanders[meander_id - 1].ind_inf_end + 1]
+            # plt.figure()
+            # plt.plot(x, y, 'b', label='Extended')
+            # plt.plot(x_m, y_m, 'k', label='Original')
+            # plt.plot(x_inf, y_inf, '--r', label='Extracted')
+            # plt.legend()
+            # plt.show()
+
+            # c_m = database_meanders['c'].iloc[i]
+            # s_m = database_meanders['s'].iloc[i]
+            # c = self.meanders[meander_id - 1].c
+            # s = self.meanders[meander_id - 1].s
+            # plt.figure()
+            # plt.plot(s[1:-1], c, 'b', label='Extended')
+            # plt.plot(s_m, c_m, 'k', label='Original')
+            # plt.axhline(0, color='k', linestyle='--')
+            # plt.legend()
+            # plt.show()
+
+
             
     
     def prune_tree_by_gamma_width(self, gamma):
@@ -3203,7 +3238,7 @@ class RiverTransect:
     def add_meander(self, id_meander, ind_start, ind_end,
                     metrics=None, sk=np.nan, fl=np.nan, automatic_flag=0,
                     inflection_flag=False, tree_id=-1, x_inf=None, y_inf=None,
-                    s_inf=None):
+                    s_inf=None, wavelength=None):
         """
         Description:
         ------------
@@ -3329,6 +3364,7 @@ class RiverTransect:
                           so=so, x_o=x_o, y_o=y_o,
                           ind_start_o=idx_start_o, ind_end_o=idx_end_o,
                           x_inf=x_inf, y_inf=y_inf, s_inf=s_inf, c=c,
+                          wavelength=wavelength,
                           metrics=metrics, sk=sk, fl=fl, comid=comid,
                           automatic_flag=automatic_flag,
                           inflection_flag=inflection_flag, tree_id=tree_id)
@@ -3534,7 +3570,8 @@ class Meander:
     def __init__(self, s, x, y, z, ind_start, ind_end,
                  c=None, sk=np.nan, fl=np.nan, comid=np.nan, x_o=None,
                  y_o=None, ind_start_o=None, ind_end_o=None,
-                 so=None, x_inf=None, y_inf=None, s_inf=None, metrics=None,
+                 so=None, x_inf=None, y_inf=None, s_inf=None,
+                 wavelength=None, metrics=None,
                  calculations=True,
                  automatic_flag=0, inflection_flag=False, tree_id=-1):
         # ----------------
@@ -3550,6 +3587,7 @@ class Meander:
         self.automatic_flag = automatic_flag
         self.tree_id = tree_id
         self.inflection_flag = inflection_flag
+        self.wavelength = wavelength
 
         self.x_o = x_o
         self.y_o = y_o
@@ -3807,7 +3845,10 @@ class Meander:
         This is an estimate of the wavelength that from data turns out to be
         2 times the arc-length of the meander (lambda).
         """
-        self.data['lambda'] = self.data['lambda_hm'] * 2
+        if self.wavelength is not None:
+            self.data['lambda'] = copy.deepcopy(self.wavelength)
+        else:
+            self.data['lambda'] = self.data['lambda_hm'] * 2
         return
     
     def calculate_radius(self):

examples/workflows/NHDPlus_HR/manual_automated_comparison/Perform_Automatic_Identification.py
@@ -63,11 +63,11 @@ logger = Logger(console=True)
 # ------------------
 # Parameters
 # ------------------
-path_projects = f'{os.getcwd()}/examples/workflows/NHDPlus_HR/manual_automated_comparison/meander_comparison/characterization/manual_corrected/'
+path_projects = f'examples/workflows/NHDPlus_HR/manual_automated_comparison/meander_comparison/characterization/manual_corrected/'
 projects = utl.get_folders(path_projects)
 projects = list(np.sort(projects))
 path_projects_all = [f'{path_projects}{p}/' for p in projects]
-path_projects_c = f'{os.getcwd()}/examples/workflows/NHDPlus_HR/manual_automated_comparison/meander_comparison/characterization/manual_with_automatic/'
+path_projects_c = f'examples/workflows/NHDPlus_HR/manual_automated_comparison/meander_comparison/characterization/manual_with_automatic/'
 path_projects_out = [f'{path_projects_c}{p}/' for p in projects]
 print(projects)
 # Current projection of information
@@ -125,7 +125,7 @@ for project in projects:
     # -----------------------------
     # Prune by sinuosity
     # -----------------------------
-    rivers[id_river].prune_tree_by_sinuosity(1.1)
+    # rivers[id_river].prune_tree_by_sinuosity(1.05)
     # -----------------------------
     # Add meander to database
     # -----------------------------