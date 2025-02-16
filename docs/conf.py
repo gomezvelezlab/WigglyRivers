0ba823304690ab0174318a4e71f8bf1d09d8ee3e
Daniel Gonzalez-Duque <daniel.gonzalez@vanderbilt.edu>
2025-02-16 03:13:24 -0500

    Update conf.py file

--- docs/conf.py
+++ docs/conf.py
@@ -6,6 +6,10 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../WigglyRivers'))
+
 project = "WigglyRivers"
 copyright = "2025, Daniel Gonzalez-Duque, Jesus Gomez-Velez"
 author = "Daniel Gonzalez-Duque, Jesus Gomez-Velez"
